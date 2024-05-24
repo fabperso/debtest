7b26f373c19f3c0f253979772ed0663e3ffb31c6
fabrice.andy@outlook.com <fabrice.andy@outlook.com>
2024-05-24 08:20:24 +0000

    src customers

--- models/stg_customers.sql
+++ models/stg_customers.sql
@@ -1,2 +1,14 @@
-{{ config(materialized='table')}}
-select * from snowflake_sample_data.tpch_sf1.customer
+WITH raw_customers as (
+    SELECT * FROM {{SOURCE('SNOWFLAKE_SAMPLE_DATA','CUSTOMER')}}
+)
+SELECT
+    c_CUSTKEY as customer_id,
+    C_NAME,
+    C_ADDRESS,
+    C_NATIONKEY,
+    C_PHONE,
+    C_ACCTBAL,
+    C_MKTSEGMENT,
+    C_COMMENT
+FROM
+    raw_customers
